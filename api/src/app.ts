869af9b04d80702958a25e9323219e4e1dc41ffc
José Brito <josealbritose@gmail.com>
2024-04-04 07:12:09 -0300

    CorsConfig

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express';
-import cors from 'cors'
+import cors, {CorsOptions} from 'cors'
 import morgan from 'morgan'
 import mainRouter from './routes';
 
 const app = express();
 
+const corsOptions:CorsOptions = {
+  origin:function(origin,callback){
+    if(origin === process.env.FRONTEND_URL){
+      console.log('Allowed...')
+      callback(null,true);
+    } else {
+      console.log('Not allowed...')
+      callback(new Error(`Cors error`))
+    }
+  }
+}
+
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/', mainRouter );
 
