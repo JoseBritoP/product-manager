9a1586f97cddf595080850503619d957ea9965e9
José Brito <josealbritose@gmail.com>
2024-04-04 09:10:52 -0300

    EditProductView

--- client/src/views/index.ts
+++ client/src/views/index.ts
@@ -2,6 +2,7 @@ import Home from "./Home";
 import Error from "./Error";
 import Products from "./Products";
 import ProductForm from "./ProductForm";
+import EditProductForm from "./EditProductView";
 export {
-  Error,Home,Products,ProductForm
+  Error,Home,Products,ProductForm,EditProductForm
 }
