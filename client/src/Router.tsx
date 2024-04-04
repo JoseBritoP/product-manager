cfa7383e1c444a260ba8df0620933ccd004c1f88
José Brito <josealbritose@gmail.com>
2024-04-04 09:10:31 -0300

    Loader product by id - action editProduct

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home, Error, Products, ProductForm } from "./views";
+import { Home, Error, Products, ProductForm, EditProductForm } from "./views";
 import { action as NewProductAction } from "./components/Form/Form";
 import { loader as productsLoader } from "./views/Products";
+import { loader as editProductLoader } from "./components/Form/EditProducFormt";
+import { action as editProductAction } from "./components/Form/EditProducFormt";
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +19,12 @@ export const router = createBrowserRouter([
         path:'/product/new',
         element:<ProductForm/>,
         action:NewProductAction
+      },
+      {
+        path:'/product/:id/edit',
+        element:<EditProductForm/>,
+        loader:editProductLoader,
+        action:editProductAction
       }
     ]
   },
