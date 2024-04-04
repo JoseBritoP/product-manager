ded73d5987d54a8484f91db76e5f592d88500bf2
José Brito <josealbritose@gmail.com>
2024-04-04 09:10:14 -0300

    GET Product By Id - Edit Product endpoints

--- client/src/server/Product.endpoints.ts
+++ client/src/server/Product.endpoints.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { safeParse } from "valibot"
-import { DraftProductSchema, Product, ProductsSchema } from "../types"
+import { DraftProductSchema, Product, ProductSchema, ProductsSchema } from "../types"
 
 type ProductData = {
   [k: string]: FormDataEntryValue
@@ -25,11 +25,43 @@ export async function addProduct (data:ProductData){
   }
 }
 
+export async function editProduct(id:Product['id'],data:ProductData){
+  try {
+    const result = safeParse(DraftProductSchema,{
+      name:data.name,
+      price:+data.price
+    });
+    if(result.success){
+      const url = `${import.meta.env.VITE_API_URL}/product/${id}`;
+      await axios.put(url,{
+        name:result.output.name,
+        price:result.output.price
+      });
+    } else throw new Error(`Data invalid`)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export async function getProducts(){
   try {
     const url = `${import.meta.env.VITE_API_URL}/product`;
     const { data } = await axios.get(url);
     const result = safeParse(ProductsSchema,data)
+    if(result.success) return result.output;
+    else throw new Error(`No products...`)
+      
+  } catch (error) {
+    console.log('Error') 
+  }
+}
+
+export async function getProductById(id:Product['id']){
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/product/${id}`;
+    const { data } = await axios.get(url);
+    const result = safeParse(ProductSchema,data)
     if(result.success) return result.output;
     else throw new Error(`No products...`)
       
